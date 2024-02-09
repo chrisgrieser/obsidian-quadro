8f4a8177c9d6081b8154edadcba7290caf5eed7b
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-02-09 19:24:04 +0100

    refactor: command structure

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,44 @@
-import { Plugin } from "obsidian";
+import { Hotkey, Plugin } from "obsidian";
 import { assignCode } from "./assign-code";
 import { unAssignCode } from "./unassign-code";
 
 export default class Quadro extends Plugin {
 	onload() {
-		console.info(this.manifest.name + " Plugin loaded.");
-
+		const commands = [
+			{
+				id: "assign-code",
+				name: "Assign code to paragraph",
+				func: assignCode,
+				hotkey: { modifiers: ["Mod", "Shift"], key: "a" },
+				icon: "plus-circle",
+			},
+			{
+				id: "unassign-code",
+				name: "Remove code from paragraph",
+				func: unAssignCode,
+				hotkey: { modifiers: ["Mod", "Shift"], key: "r" },
+				icon: "minus-circle",
+			},
+		];
 		// INFO Adding a hotkey by default, since this plugin is going to be
 		// used by many people not familiar with Obsidian. Requiring them to
 		// add an hotkey would unnecessarily complicate the onboarding for
 		// them. We are, however, using combinations that are unlikely to
 		// conflict with other plugins with other hotkeys.
-		const assignCmdName = "Assign code to paragraph";
-		this.addRibbonIcon("plus-circle", `Quadro: ${assignCmdName}`, () => assignCode(this.app));
-		this.addCommand({
-			id: "assign-code",
-			name: assignCmdName,
-			editorCallback: () => assignCode(this.app),
-			hotkeys: [{ modifiers: ["Mod", "Shift"], key: "a" }],
-		});
+		// INFO Wrap existing Obsidian commands in new commands to make it more
+		// accessible for users new to Obsidian.
 
-		const removeCmdName = "Remove code from paragraph";
-		this.addRibbonIcon("minus-circle", `Quadro: ${removeCmdName}`, () => unAssignCode(this.app));
-		this.addCommand({
-			id: "unassign-code",
-			name: removeCmdName,
-			editorCallback: () => unAssignCode(this.app),
-			hotkeys: [{ modifiers: ["Mod", "Shift"], key: "r" }],
-		});
+		for (const cmd of commands) {
+			this.addRibbonIcon(cmd.icon, `Quadro: ${cmd.name}`, () => assignCode(this.app));
+			this.addCommand({
+				id: cmd.id,
+				name: cmd.name,
+				editorCallback: () => cmd.func(this.app),
+				hotkeys: [cmd.hotkey as Hotkey],
+			});
+		}
+
+		console.info(this.manifest.name + " Plugin loaded.");
 	}
 
 	onunload() {
