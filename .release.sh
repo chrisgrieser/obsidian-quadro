ba3211aab103bdaf8b412b272d465078200b3a9a
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-02-14 17:47:17 +0100

    ci: improve release automation

--- .release.sh
+++ .release.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env zsh
-# Release Obsidian Plugin
+# RELEASE OBSIDIAN PLUGIN
 # https://forum.obsidian.md/t/using-github-actions-to-release-plugins/7877
 # https://marcus.se.net/obsidian-plugin-docs/publishing/release-your-plugin-with-github-actions
-
 #───────────────────────────────────────────────────────────────────────────────
 
 # Prompt for version number
@@ -12,26 +11,30 @@ echo -n "   next version: "
 read -r nextVersion
 echo "────────────────────────"
 
+# GUARD
+if [[ -z "$nextVersion" || "$nextVersion" == "$currentVersion" ]]; then
+	print "\033[1;31mInvalid version number\033[0m"
+	exit 1
+fi
+
 # set version number in `manifest.json`
 sed -E -i '' "s/\"version\".*/\"version\": \"$nextVersion\",/" "manifest.json"
 sed -E -i '' "s/\"version\".*/\"version\": \"$nextVersion\",/" "package.json"
 
-# add version number in `versions.json`, assuming same compatibility
+# add version number in `versions.json` (assuming same compatibility)
 grep -Ev "^$" "versions.json" | grep -v "}" | sed -e '$ d' >temp
 minObsidianVersion=$(grep -Ev "^$" "versions.json" | grep -v "}" | tail -n1 | cut -d\" -f4)
 {
-	echo "  \"$currentVersion\": \"$minObsidianVersion\","
-	echo "  \"$nextVersion\": \"$minObsidianVersion\""
+	print "\t\"$currentVersion\": \"$minObsidianVersion\","
+	print "\t\"$nextVersion\": \"$minObsidianVersion\""
 	echo "}"
 } >>temp
 mv temp versions.json
 
 #───────────────────────────────────────────────────────────────────────────────
 
-# push the manifest and versions JSONs
-git add -A && git commit -m "release: $nextVersion"
-git pull && git push
-
-# trigger the release action
-git tag "$nextVersion"
-git push origin --tags
+# push the manifest.json and versions.json
+git add --all &&
+	git commit -m "release: $nextVersion" &&
+	git pull && git push &&
+	git tag "$nextVersion" && git push origin --tags # trigger the release action
