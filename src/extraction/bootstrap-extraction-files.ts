de1949b54a499e95659fddf264e162d012e0abe0
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-02-25 19:17:05 +0100

    chore: cleanup

--- src/extraction/bootstrap-extraction-files.ts
+++ src/extraction/bootstrap-extraction-files.ts
@@ -77,6 +77,7 @@ export async function bootstrapExtractionTemplate(app: App, newExtractionTypeNam
 	const dataloomBugInfo = [
 		"> [!NOTE] ",
 		'Due to a bug in DataLoom, properties of the type "List" must not have a space in them. You can work around that issue by using underscros or hyphens instead of a space.',
+		"",
 		"See <https://github.com/trey-wallis/obsidian-dataloom/issues/932>",
 	].join("\n");
 
