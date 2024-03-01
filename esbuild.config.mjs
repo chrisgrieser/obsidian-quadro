853a2aa4a4830938332def90815da6ff6998913c
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-01 07:10:55 +0100

    chore: bump to `es2022`

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -17,7 +17,7 @@ esbuild
 		bundle: true,
 		external: ["obsidian", "electron", ...builtins],
 		format: "cjs",
-		target: "es2021",
+		target: "es2022",
 		logLevel: "info",
 		sourcemap: false,
 		treeShaking: true,
