388c592804a675f06ec08cc02d68fc49ac675a67
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-12 15:21:33 +0100

    fix: typo

--- src/extraction/bootstrap-extraction-files.ts
+++ src/extraction/bootstrap-extraction-files.ts
@@ -73,7 +73,7 @@ export async function bootstrapExtractionTemplate(plugin: Quadro, newExtractionT
 	// https://github.com/trey-wallis/obsidian-dataloom/issues/932
 	const dataloomBugInfo = [
 		"> [!NOTE] ",
-		'Due to a bug in DataLoom, properties of the type "List" must not have a space in them. You can work around that issue by using underscros or hyphens instead of a space.',
+		'Due to a bug in DataLoom, properties of the type "List" must not have a space in them. You can work around that issue by using underscores or hyphens instead of a space.',
 		"",
 		"See <https://github.com/trey-wallis/obsidian-dataloom/issues/932>",
 	].join("\n");
