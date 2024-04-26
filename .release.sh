3991c8432606dbdb43519233faa2d398c12c6898
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-04-26 13:13:27 +0200

    build: update release script

--- .release.sh
+++ .release.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env zsh
-# RELEASE OBSIDIAN PLUGIN
-# https://forum.obsidian.md/t/using-github-actions-to-release-plugins/7877
-# https://docs.obsidian.md/Plugins/Releasing/Release+your+plugin+with+GitHub+Actions
-#───────────────────────────────────────────────────────────────────────────────
+# REQUIRED macOS version of `sed`
+# RELEASE OBSIDIAN PLUGIN https://docs.obsidian.md/Plugins/Releasing/Release+your+plugin+with+GitHub+Actions
 
-# Prompt for version number
+#───────────────────────────────────────────────────────────────────────────────
+# PROMPT FOR VERSION NUMBER
+
 currentVersion=$(grep "version" "./manifest.json" | cut -d\" -f4)
 echo "current version: $currentVersion"
 echo -n "   next version: "
 read -r nextVersion
-
-# GUARD
 if [[ -z "$nextVersion" || "$nextVersion" == "$currentVersion" ]]; then
 	print "\033[1;31mInvalid version number\033[0m"
-	exit 1
+	return 1
 fi
 
-# set version number in `manifest.json`
+#───────────────────────────────────────────────────────────────────────────────
+# UPDATE VERSION NUMBER
+
+# manifest.json & package.json
 sed -E -i '' "s/\"version\".*/\"version\": \"$nextVersion\",/" "manifest.json"
 sed -E -i '' "s/\"version\".*/\"version\": \"$nextVersion\",/" "package.json"
 
-# add version number in `versions.json` with min-app-version
+# package-lock.json: can be updated via `npm`
+npm install &>/dev/null
+
+# versions.json: insert version number with min-app-version from manifest
 minObsidianVersion=$(grep "minAppVersion" "manifest.json" | cut -d\" -f4)
 last_version_line=$(tail -n2 "versions.json" | head -n1)
 sed -i '' '$d' "versions.json"
@@ -32,8 +36,8 @@ sed -i '' '$d' "versions.json"
 } >>"versions.json"
 
 #───────────────────────────────────────────────────────────────────────────────
+# GIT ADD, COMMIT, AND PUSH
 
-# push the manifest.json and versions.json
 echo "────────────────────────"
 git add "manifest.json" "versions.json" "package.json" &&
 	git commit -m "release: $nextVersion" &&
