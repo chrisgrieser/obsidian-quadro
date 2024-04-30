56a1b682180a26a1d2a7c28ac3d5ce2f5776e60d
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-04-30 18:21:56 +0200

    feat: new command - `Merge extractions`

--- src/extraction/merge-extractions.ts
+++ src/extraction/merge-extractions.ts
@@ -1,6 +1,71 @@
-import { Notice } from "obsidian";
+import { Notice, TFile, TFolder } from "obsidian";
+import { setupTrashWatcher } from "src/deletion-watcher";
 import Quadro from "src/main";
+import { ExtendedFuzzySuggester } from "src/shared/modals";
+import { getActiveEditor, typeOfFile } from "src/shared/utils";
+import { countExtractionsForType } from "./extraction-utils";
 
-export function mergeExtractionFilesCommand(_plugin: Quadro) {
-	new Notice("WIP");
+class SuggesterForExtractionMerging extends ExtendedFuzzySuggester<TFile> {
+	toBeMergedFile: TFile;
+	constructor(plugin: Quadro, toBeMergedFile: TFile) {
+		super(plugin);
+		this.toBeMergedFile = toBeMergedFile;
+		this.setPlaceholder(`Select Extraction File to merge "${toBeMergedFile.basename}" into`);
+	}
+
+	getItems(): TFile[] {
+		const extractionType = this.toBeMergedFile.parent as TFolder;
+		const extractionsOfSameType = (
+			extractionType.children.filter(
+				(ch) =>
+					ch instanceof TFile &&
+					ch.extension === "md" &&
+					ch.name !== "Template.md" &&
+					ch.path !== this.toBeMergedFile.path,
+			) as TFile[]
+		).sort((a, b) => b.stat.mtime - a.stat.mtime);
+		return extractionsOfSameType;
+	}
+
+	getItemText(extractionFile: TFile): string {
+		// TODO better display of extraction information here?
+		return extractionFile.basename;
+	}
+	async onChooseItem(toMergeInFile: TFile) {
+		const { plugin, app } = this;
+
+		// MERGE (via Obsidian API)
+		// INFO temporarily disable trashWatcher, as the merge operation trashes
+		// the toBeMergedFile file, triggering an unwanted removal of references
+		if (plugin.trashWatcherUninstaller) plugin.trashWatcherUninstaller();
+		await app.fileManager.mergeFile(toMergeInFile, this.toBeMergedFile, "", false);
+		plugin.trashWatcherUninstaller = setupTrashWatcher(plugin);
+		const mergedFile = toMergeInFile;
+
+		// cleanup merged file
+		const newFileContent = (await app.vault.read(mergedFile))
+			.replaceAll("**Paragraph extracted from:**\n", "")
+			.replaceAll("\n\n\n", "\n");
+		// timeout needed, so embeds are loaded (and there is no `await` for that)
+		setTimeout(async () => await app.vault.modify(mergedFile, newFileContent), 200);
+
+		new Notice(`"${this.toBeMergedFile.basename}" merged into "${toMergeInFile.basename}".`, 4000);
+	}
+}
+
+export function mergeExtractionFilesCommand(plugin: Quadro) {
+	const editor = getActiveEditor(plugin.app);
+	if (!editor) return;
+	if (typeOfFile(plugin) !== "Extraction File") {
+		new Notice("You must be in the Extraction File for this.", 3000);
+		return;
+	}
+
+	const toBeMergedFile = editor.editorComponent.view.file;
+	const extractionsMade = countExtractionsForType(toBeMergedFile.parent as TFolder);
+	if (extractionsMade < 2) {
+		new Notice("There must be at least two extractions of the same type to merge.", 4000);
+		return;
+	}
+	new SuggesterForExtractionMerging(plugin, toBeMergedFile).open();
 }
